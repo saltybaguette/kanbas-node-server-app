1bb7a9fba813ce78a15e42fd13ad3b28fbfa8bc3
briangay <gay.b@northeastern.edu>
2023-11-30 13:06:38 -0500

    a6

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 import express from 'express';
 import Hello from "./hello.js";
+
 import "dotenv/config";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import session from "express-session";
+mongoose.connect("mongodb://localhost:27017/kanbas");
+
 const app = express()
-app.use(cors());
+app.use(cors({credentials: true,
+    origin: process.env.FRONTEND_URL}));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(
+    session(sessionOptions)
+);
+
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app)
